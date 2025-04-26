7e5fb5ce8105f28e4157e2b55f0ed1b58f420024
LukeSliwDeveloper <luke.sliw.developer@gmail.com>
2025-04-26 09:11:40 +0300

    finish menu and settings mockup

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class GameSettings : MonoBehaviour
 {
-    
+    [SerializeField] private Animator _animator;
+
+    private int _animatorHash = Animator.StringToHash("Visible");
+
+    private bool _isMainPanelShown;
+
+    public void ChangePanelShown()
+    {
+        _animator.SetBool(_animatorHash, _isMainPanelShown = !_isMainPanelShown);
+    }
 }
