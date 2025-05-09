9ed241a298935855973fb8fdd2ecad7ae196dded
LukeSliwDeveloper <luke.sliw.developer@gmail.com>
2025-05-09 23:31:29 +0300

    add data

--- Assets/Scripts/Encounter.cs
+++ Assets/Scripts/Encounter.cs
@@ -1,10 +1,11 @@
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class Encounter : ScriptableObject
+public class Encounter : SerializedScriptableObject
 {
-    [field: SerializeField] public FlowerType[] RequiredFlowers { get; private set; }
-    [field: SerializeField] public Sprite CharacterSprite { get; private set; }
+    [field: SerializeField] public Material CharacterMaterial { get; private set; }
     [field: SerializeField] public string Dialogue { get; private set; }
-    [field: SerializeField] public Encounter[] FollowingEncounters { get; private set; }
+    [field: SerializeField] public Dictionary<FlowerType[], Encounter> FollowingEncounters { get; private set; }
 }
